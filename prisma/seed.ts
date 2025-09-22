6f3986d162808736ab7df0a44d78d15a69f5e3ce
Zarak Khattak <khattakzarakj@outlook.com>
2025-09-22 12:46:21 +0500

    fix: lint error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,7 @@
+import { faker } from "@faker-js/faker";
+
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
-import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
